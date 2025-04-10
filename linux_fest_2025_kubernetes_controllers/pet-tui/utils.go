333ba8d96c965ef840f8f377c7854b75c9dced0c
itzloop <sina.sh.k78@gmail.com>
2025-04-11 02:44:03 +0330

    changes controller structure

--- linux_fest_2025_kubernetes_controllers/pet-tui/utils.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "strings"
-
-// bar creates a visual representation of a value
-func bar(val int) string {
-	full := val / 10
-	return strings.Repeat("█", full) + strings.Repeat("░", 10-full)
-}
